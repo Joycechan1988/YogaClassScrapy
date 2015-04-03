3fcf36f43a49ce483d103d05d86d5db6c8c3782e
hchen11 <hchen11@ebay.com>
2015-04-03 15:58:18 +0800

    still need to figure out how to generate ajax url

--- myScrapy/spiders/yl.py
+++ myScrapy/spiders/yl.py
@@ -3,18 +3,30 @@ __author__ = 'hchen11'
 from scrapy.spider import Spider
 from scrapy import log, Request
 
+
 class ylSpider(Spider):
     name = 'yl'
-    allowed_domains = ["http://3.util.sinaapp.com/yl/"]
+    allowed_domains = ['*']
     start_urls = ["http://3.util.sinaapp.com/yl/"]
+    day = ''
 
     def make_requests_from_url(self, url):
-        return Request(url, cookies={'saeut':'216.113.160.68.1427679656108478','PHPSESSID':'dc3a630f4ab64aa59681e17ebaff3a44'}, dont_filter=True)
+        return Request(url, cookies={'saeut':'216.113.160.68.1427679656108478','PHPSESSID':'87094be5a61f22448212a9bd3ecabad8'}, dont_filter=True)
 
     def parse(self, response):
-        log.msg("start parse", level=log.INFO)
+        urllist = []
+        for url in response.xpath('//a[@href]/@href').extract():
+            if "day=2015" in url:
+                urllist.append(url)
+        url = urllist.pop(3)
+        self.day = url[14:]
+        url= "http://3.util.sinaapp.com/yl/"+url
+        yield Request(url, cookies={'saeut':'216.113.160.68.1427679656108478','PHPSESSID':'87094be5a61f22448212a9bd3ecabad8'}, dont_filter=True, callback=self.parseurl)
+
+
+    def parseurl(self, response):
+        url = ajax_template.format()
+        yield Request(url="http://112.124.111.116:8080/wx/appointment/list.action", dont_filter=True, callback=self.parseJS)
+
+    def parseJS(self, response):
         log.msg(response.body)
-        ##todo
-        log.msg("end parse", level=log.INFO)
-
-
